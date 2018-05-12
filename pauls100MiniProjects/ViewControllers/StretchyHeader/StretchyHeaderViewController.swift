f27301d8daf19b5da90ada49c501a83dab427efa
Paul Pires <paulpires101@gmail.com>
2018-05-12 18:50:48 +0100

    fine tuning stretchy header... spent waaaay to much time on this lol

--- pauls100MiniProjects/ViewControllers/StretchyHeader/StretchyHeaderViewController.swift
+++ pauls100MiniProjects/ViewControllers/StretchyHeader/StretchyHeaderViewController.swift
@@ -8,22 +8,29 @@
 
 import UIKit
 
-class StretchyHeaderViewController: UIViewController, UIScrollViewDelegate, UITableViewDelegate {
+class StretchyHeaderViewController: UIViewController, UIScrollViewDelegate, UITextViewDelegate {
 
     @IBOutlet weak var headerView: UIView!
-    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var textView: UITextView!
     let offset_HeaderStop: CGFloat = 40.0
-    @IBOutlet weak var tableView: UITableView!
-
+    
     init() {
         super.init(nibName: nil, bundle: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        scrollView.delegate = self
-        tableView.delegate = self
         navigationController?.makeTransparent()
+        textView.textContainerInset = UIEdgeInsetsMake(140, 0, 0, 0)
+        textView.delegate = self
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -33,26 +40,28 @@ class StretchyHeaderViewController: UIViewController, UIScrollViewDelegate, UITa
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         
         let offset = scrollView.contentOffset.y
-        var headerTransform = CATransform3DIdentity
-        var tableViewTransform = CATransform3DIdentity
-        
-        if offset < 0 {
-            
-            let scaleFactor: CGFloat = abs(offset) / headerView.bounds.height
-            let headerHeightIncrease = headerView.bounds.height * (1.0 + scaleFactor) - headerView.bounds.height
-            let yVariation = headerHeightIncrease / 2
-            
-            headerTransform = CATransform3DTranslate(headerTransform, 0, yVariation, 0)
-            headerTransform = CATransform3DScale(headerTransform, 1.0 + scaleFactor, 1.0 + scaleFactor, 0)
-            headerView.layer.transform = headerTransform
-            
-            tableViewTransform = CATransform3DTranslate(tableViewTransform, 0, abs(offset), 0)
-            tableView.layer.transform = tableViewTransform
-            
-        } else {
-            headerTransform = CATransform3DTranslate(headerTransform, 0, -offset, 0)
-            headerView.layer.transform = headerTransform
-        }
+        print(offset)
+//        var headerTransform = CATransform3DIdentity
+////        var tableViewTransform = CATransform3DIdentity
+//
+//        if offset < 0 {
+//            print(offset)
+//            let scaleFactor: CGFloat = abs(offset) / headerView.bounds.height
+//            let headerHeightIncrease = headerView.bounds.height * (1.0 + scaleFactor) - headerView.bounds.height
+//            let yVariation = headerHeightIncrease / 2
+//
+//            headerTransform = CATransform3DTranslate(headerTransform, 0, yVariation, 0)
+//            headerTransform = CATransform3DScale(headerTransform, 1.0 + scaleFactor, 1.0 + scaleFactor, 0)
+//            headerView.layer.transform = headerTransform
+//
+//////            tableViewTransform = CATransform3DTranslate(tableViewTransform, 0, abs(offset), 0)
+//////            textView.layer.transform = tableViewTransform
+//
+//        } else {
+//            print("positive")
+//            headerTransform = CATransform3DTranslate(headerTransform, 0, -offset, 0)
+//            headerView.layer.transform = headerTransform
+//        }
     }
 }
 
